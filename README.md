6e891b88cb536a822daaa343abf245065aced90c
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2022-03-02 10:23:59 -0500

    Initial commit with RemoveBuggyResourcesStep

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# extra-sharp-trimmer
-Extra Trimmer Steps for .net
+
+## Extra Steps
+
+### RemoveBuggyResourcesStep
+
+This step is a (hopefully) temporary workaround for the following issues:
+* https://github.com/xamarin/xamarin-macios/issues/14257
+* https://github.com/dotnet/linker/issues/2661
+
+This affects most of net6 iOS/tvOS/macOS applications since they all uses
+the new `NFloat` type and might not use anything else from the
+`System.Runtime.InteropServices.dll` assembly. Note that this situation
+could also happen on other assemblies as well.
+
+To enable this step inside your project you need to add the following
+snippet inside your `.csproj` file:
+
+```
+<ItemGroup>
+  <_TrimmerCustomSteps Include="/full/path/to/the/extra-sharp-trimmer.dll" BeforeStep="OutputStep" Type="ExtraSteps.RemoveBuggyResourcesStep" />
+```
+
+This will run the `RemoveBuggyResourcesStep` step before the `OutputStep`
+step, giving it a chance to remove the extra, non-required resources from
+the assemblies.
+
+The removal of the resources saves 3KB for `System.Runtime.InteropServices.dll`
+which goes from 7.5 KB to 4.5 KB. That's still a lot for a single type-forwarder
+but that's sadly the minimal price to include another assembly inside an app.
---
+++ RemoveBuggyResourcesStep.cs
@@ -0,0 +1,50 @@
+﻿using Mono.Cecil;
+using Mono.Linker;
+using Mono.Linker.Steps;
+
+namespace ExtraSteps {
+
+	public class RemoveBuggyResourcesStep : BaseStep {
+	
+        protected override void ProcessAssembly(AssemblyDefinition assembly)
+		{
+			if (Annotations.GetAction (assembly) != AssemblyAction.Link)
+				return;
+
+			var module = assembly.MainModule;
+			if (!module.HasResources)
+				return;
+			
+			var resources = module.Resources;
+			for (int i = 0; i < resources.Count; i++) {
+				var resource = resources [i];
+
+				if (resource is not EmbeddedResource)
+					continue;
+
+				var name = resource.Name;
+				switch (name) {
+				case "ILLink.LinkAttributes.xml":
+				case "ILLink.Substitutions.xml":
+					// https://github.com/xamarin/xamarin-macios/issues/14257
+					Warn (name);
+					resources.RemoveAt (i--);
+					break;
+				case "FxResources.System.Runtime.InteropServices.SR.resources":
+					// due to not processing `ILLink.Substitutions.xml` (see above) the resource is not removed
+					if (assembly.Name.Name == "System.Runtime.InteropServices") {
+						Warn (name);
+						resources.RemoveAt (i--);
+					}
+					break;
+				}
+			}
+		}
+
+		void Warn (string resourceName)
+		{
+			var warning = MessageContainer.CreateCustomWarningMessage (Context, $"Resource {resourceName} should not be present inside a linked assembly at this stage.", 6001, default, WarnVersion.Latest);
+			Context.LogMessage (warning);
+		}
+	}
+}
